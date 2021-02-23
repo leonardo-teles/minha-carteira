925a48fa2a684c7af7651255e061a499c6058dfc
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-23 16:03:20 -0300

    Criando o Projeto

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const List: React.FC = () => {
+    return (
+        <h3>List</h3>
+    );
+}
+
+export default List;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignIn: React.FC = () => {
+    return (
+        <h3>SignIn</h3>
+    );
+}
+
+export default SignIn;
