7b5f1fb86fd729ffc6f90dcd0ad64e4e9750f6a2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-24 23:16:26 -0300

    Criação dos Componentes do Layout da Aplicação

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Aside: React.FC = () => {
+    return (
+        <Container>
+            <h3>Aside</h3>
+        </Container>
+    );
+}
+
+export default Aside;
---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const MainHeader: React.FC = () => {
+    return (
+        <Container>
+            <h3>MainHeader</h3>
+        </Container>
+    );
+}
+
+export default MainHeader;
