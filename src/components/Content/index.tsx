120b2c010dbe5d4a17cb3f8cde41bc885cf02b03
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-26 18:55:54 -0300

    Criando o Header de Conteúdo

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Content: React.FC = () => {
+const Content: React.FC = ({ children }) => {
     return (
         <Container>
-            <h3>Content</h3>
+            { children }
         </Container>
     );
 }
