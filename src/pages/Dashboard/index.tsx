9aa79d57928a8ef3cefebe49ce9d1a1fbbcaa45a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-02-27 00:17:40 -0300

    Criando o Componente de Select e Deixando o Header Content Dinâmico

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+
+    const options = [
+        {value: 'Leonardo', label: 'Leonardo'},
+        {value: 'Manuela', label: 'Manuela'},
+        {value: 'Shirlei', label: 'Shirlei'}
+    ];
+
     return (
         <Container>
-            <ContentHeader />
+            <ContentHeader title="Dashboard" lineColor="#F7931B">
+                <SelectInput options={options} />
+            </ContentHeader>
         </Container>
     );
 }
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+
+import { Container } from './styles';
 
 const List: React.FC = () => {
+
+    const options = [
+        {value: 'Leonardo', label: 'Leonardo'},
+        {value: 'Manuela', label: 'Manuela'},
+        {value: 'Shirlei', label: 'Shirlei'}
+    ];
+
     return (
-        <h3>List</h3>
+        <Container>
+            <ContentHeader title="Saída" lineColor="#E44C4E">
+                <SelectInput options={options} />
+            </ContentHeader>
+        </Container>
     );
 }
 
