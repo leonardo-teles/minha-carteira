5581700c00aadb7038f01dcb9bd8e33e6c64a61a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-03-04 01:52:30 -0300

    Formatando Valor e Data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,6 +7,9 @@ import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 
+import formatCurrency from '../../utils/formatCurrency';
+import formatDate from '../../utils/formatDate';
+
 import { Container, Content, Filters } from './styles';
 
 interface IRouteParams {
@@ -60,9 +63,9 @@ const List: React.FC<IRouteParams> = ({ match }) => {
             return {
                 id: String (Math.random() * data.length),
                 description: item.description,
-                amountFormatted: item.amount,
+                amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
-                dataFormatted: item.date,
+                dataFormatted: formatDate(item.date),
                 tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'            
             }
         })
